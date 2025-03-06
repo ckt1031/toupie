5718405361368865a6b2ced3621a60bb2083dc6d
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-03-06 19:56:38 +0800

    feat(relay): enhance request handling for form data and modify body structure

--- src/relay.ts
+++ src/relay.ts
@@ -1,10 +1,52 @@
 import { setResponseCORSHeaders } from "./headers";
 import { pickModelChannel } from "./models";
 
-export async function relayLLMRequest(request: Request) {
-	const body = await request.clone().json();
+function isBodyFormDataType(req: Request): boolean {
+	const contentType = req.headers.get("content-type");
 
-	const model: string | undefined = body.model;
+	if (!contentType) return false;
+
+	return contentType.includes("multipart/form-data");
+}
+
+async function getValueFromBody(req: Request, key: string) {
+	const clonedRequest = req.clone();
+	const isBodyForm = isBodyFormDataType(req);
+
+	if (isBodyForm) {
+		const body = await clonedRequest.formData();
+		return body.get(key);
+	}
+
+	const body = await clonedRequest.json();
+
+	return body[key];
+}
+
+async function modifyBodyWithStringValue(
+	req: Request,
+	name: string,
+	value: string,
+): Promise<BodyInit> {
+	const clonedRequest = req.clone();
+	const isBodyForm = isBodyFormDataType(req);
+
+	if (isBodyForm) {
+		const body = await clonedRequest.formData();
+		body.set(name, value);
+
+		return body;
+	}
+
+	const body: Record<string, string> = await clonedRequest.json();
+
+	body[name] = value;
+
+	return JSON.stringify(body);
+}
+
+export async function relayLLMRequest(request: Request) {
+	const model: string | undefined = await getValueFromBody(request, "model");
 
 	if (!model) {
 		const erorrMessage = "Model is required";
@@ -21,7 +63,11 @@ export async function relayLLMRequest(request: Request) {
 	}
 
 	// Replace request.body model with the model id as we need to cast models
-	body.model = channel.provider.model;
+	const newBody = await modifyBodyWithStringValue(
+		request,
+		"model",
+		channel.provider.model,
+	);
 
 	// Replace the baseURL with the provider's baseURL
 	let url: string;
@@ -30,6 +76,7 @@ export async function relayLLMRequest(request: Request) {
 	// remove host header to prevent DNS issue
 	headers.delete("host");
 
+	// Handle Azure provider
 	if (channel.provider.isAzure) {
 		url = request.url.replace(
 			/^https?:.*\/v1/,
@@ -46,13 +93,14 @@ export async function relayLLMRequest(request: Request) {
 
 	// Print details to console
 	console.info(
-		`Model: ${body.model}, Provider: ${channel.provider.name} (Key #${channel.apiKey.index})`,
+		`Model: ${channel.provider.model}, Provider: ${channel.provider.name} (Key #${channel.apiKey.index})`,
 	);
 
+	// Request to the provider
 	const modifiedRequest = new Request(url, {
 		headers,
 		method: request.method,
-		body: JSON.stringify(body),
+		body: newBody,
 		redirect: "follow",
 	});
 
