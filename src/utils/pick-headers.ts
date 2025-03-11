e97bdd5338933fcd0cf51d6429c68d4912c819b2
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-03-11 12:19:13 +0800

    refactor(pick-headers): optimize header selection logic for improved performance and clarity

--- src/utils/pick-headers.ts
+++ src/utils/pick-headers.ts
@@ -1,16 +1,26 @@
 const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
-	const picked = new Headers();
+  const picked = new Headers();
+  const regexKeys: RegExp[] = [];
+  const stringKeys: Set<string> = new Set();
 
-	for (const key of headers.keys()) {
-		if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-			const value = headers.get(key);
-			if (typeof value === "string") {
-				picked.set(key, value);
-			}
-		}
-	}
+  for (const key of keys) {
+    if (typeof key === "string") {
+      stringKeys.add(key);
+    } else {
+      regexKeys.push(key);
+    }
+  }
 
-	return picked;
+  for (const key of headers.keys()) {
+    if (stringKeys.has(key) || regexKeys.some((regex) => regex.test(key))) {
+      const value = headers.get(key);
+      if (typeof value === "string") {
+        picked.set(key, value);
+      }
+    }
+  }
+
+  return picked;
 };
 
 export default pickHeaders;
