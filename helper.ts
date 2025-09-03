35b9648707979edba8aeacbb1f5d439a17349d45
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-09-03 23:32:46 +0800

    refactor: move helper.ts to scripts directory and update references

--- helper.ts
+++
@@ -1,5 +0,0 @@
-import { loadApiConfig } from "./src/helpers/api-config";
-import { displayMenu } from "./src/helpers/cli-utils";
-
-const initialConfig = await loadApiConfig();
-displayMenu(initialConfig);
---
+++ scripts/helper.ts
@@ -0,0 +1,5 @@
+import { loadApiConfig } from "../src/helpers/api-config";
+import { displayMenu } from "../src/helpers/cli-utils";
+
+const initialConfig = await loadApiConfig();
+displayMenu(initialConfig);
